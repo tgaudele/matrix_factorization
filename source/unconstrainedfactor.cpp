22e28010db9b702da28e8920b44f9e5792421452
Thomas <thomas.gaudelet.16@ucl.ac.uk>
2020-04-15 14:22:49 +0200

    plop

--- source/unconstrainedfactor.cpp
+++
@@ -1,8 +0,0 @@
-#include "unconstrainedfactor.h"
-
-void unconstrainedFactor::step(double lr)
-{
-    for (objectives::iterator O=Os.begin(); O!=Os.end();++O) {
-        (*O)->computeLossDerivatives(weights);
-    }
-}
